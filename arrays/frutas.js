6b6cc6b3ea7b0161824d9183d2cdfd53031d8bb6
Oscar Bolanos <100238719+ob-instaleap@users.noreply.github.com>
2022-05-23 15:38:27 -0500

    update
    update

---
+++ arrays/frutas.js
@@ -0,0 +1,22 @@
+/* let frutas = ["camandula","fresa","banana","mango","mangostino","physalis","pera"];
+
+document.write(frutas[5]) */
+
+let pc1 = {
+    nombre: "CrackMaster2022",
+    procesador: "Intel Core i7 de 10th Generacion",
+    ram: "16GB",
+    almacenamiento: "2TB"    
+};
+
+let nombre = pc1["nombre"];
+let core = pc1["procesador"];
+let ram = pc1["ram"];
+let gigas = pc1["almacenamiento"];
+
+frase = `el nombre de mi pc es: ${nombre} <br> 
+         el procesador es: ${procesador} <br>
+         la memoria ram es: ${ram} <br>
+         el almacenamiento de mi compu es: ${gigas}`;
+
+document.write(frase)
