747237003d98c1c0db544c2e2e63cb450d38905b
Oscar Bolanos <100238719+ob-instaleap@users.noreply.github.com>
2022-05-25 14:37:52 -0500

    udpate
    update

--- arrays/frutas.js
+++ arrays/frutas.js
@@ -1,6 +1,6 @@
 /* let frutas = ["camandula","fresa","banana","mango","mangostino","physalis","pera"];
 
-document.write(frutas[5]) */
+document.write(frutas[5]) 
 
 let pc1 = {
     nombre: "CrackMaster2022",
@@ -14,9 +14,29 @@ let core = pc1["procesador"];
 let ram = pc1["ram"];
 let gigas = pc1["almacenamiento"];
 
-frase = `el nombre de mi pc es: ${nombre} <br> 
-         el procesador es: ${core} <br>
-         la memoria ram es: ${ram} <br>
-         el almacenamiento de mi compu es: ${gigas}`;
+frase = `el nombre de mi pc es: <b>${nombre}</b> <br> 
+         el procesador es: <b>${core}</b> <br>
+         la memoria ram es: <b>${ram}</b> <br>
+         el almacenamiento de mi compu es: <b>${gigas}</b>`;
 
 document.write(frase)
+
+
+let numeroparasumar = 0;
+
+
+while (numeroparasumar < 1000) {
+    numeroparasumar++;
+    document.write(numeroparasumar + "<br>");
+    if (numeroparasumar == 10){
+        break;
+    }
+}
+
+document.write("FIN")
+*/
+
+
+for (let i = 0; i <= 6; i--) {
+    document.write(i + "<br>")
+}
